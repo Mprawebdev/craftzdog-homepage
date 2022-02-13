81c953435b653022fcb75439a29ec98164cccf06
Mprawebdev <99563766+Mprawebdev@users.noreply.github.com>
2022-02-13 17:19:23 +0700

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,16 +15,16 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="description" content="Mpratama homepage" />
+        <meta name="author" content="Mochamad Pratama Mukti" />
+        <meta name="author" content="Mpratama" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
+        <meta name="twitter:site" content="@Mpratama263" />
+        <meta name="twitter:creator" content="@Mpratama263" />
         <meta name="twitter:image" content="/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama's Homepage" />
+        <meta property="og:site_name" content="Mochamad Pratama Mukti Homepage" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/card.png" />
         <title>Takuya Matsuyama - Homepage</title>
